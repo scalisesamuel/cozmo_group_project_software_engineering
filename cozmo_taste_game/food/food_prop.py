95b1e69fe9aa7f33d05403737da9bf44a6e4013f
scalisesamuel <55895719+scalisesamuel@users.noreply.github.com>
2019-09-27 15:10:56 -0500

    Add files via upload

---
+++ cozmo_taste_game/food/food_prop.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class FoodProp(ABC):
+    """Abstract Food Prop interface."""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @property
+    def food_groups(self):
+        pass
+
+    @property
+    @abstractmethod
+    def color(self):
+        pass
